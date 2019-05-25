ab548c2cf61157317fcd6431e1f8a67558cb9444
Bayron Ordoñez <bjbuitron@unicauca.edu.co>
2019-05-24 20:01:39 -0500

    Prueba AngularJs Cluster CREATIC

---
+++ app/view1/view1.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  })
+  .when('/home', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope', '$location', function($scope, $location) {
+  $scope.goform = function ( path ) {
+    $location.path( path );
+  };
+}]);
---
+++ app/view2/view2.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('myApp.view2', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/formulario', {
+    templateUrl: 'view2/view2.html',
+    controller: 'View2Ctrl'
+  });
+}])
+
+.controller('View2Ctrl', ['$scope', function($scope) {
+
+  $scope.master = {};
+
+  $scope.update = function(user) {
+    $scope.master = angular.copy(user);
+  };
+
+  $scope.reset = function(form) {
+    if (form) {
+      form.$setPristine();
+      form.$setUntouched();
+    }
+    $scope.user = angular.copy($scope.master);
+  };
+
+  $scope.reset();
+}]);
